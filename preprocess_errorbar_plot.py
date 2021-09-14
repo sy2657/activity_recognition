23d9da3ac48c7013ddb6c4a0bf0a6851235872c7
sy2657 <60480017+sy2657@users.noreply.github.com>
2021-09-13 22:26:01 -0400

    Create preprocess_errorbar_plot.py

---
+++ preprocess_errorbar_plot.py
@@ -0,0 +1,44 @@
+open_close_x = x_allperson_aved
+open_close_y = y_allperson_aved
+open_close_z = z_allperson_aved
+
+open_close_x_std = x_std_aved
+open_close_y_std = y_std_aved
+open_close_z_std = z_std_aved
+
+screen_int_x = x_allperson_aved
+screen_int_y = y_allperson_aved
+screen_int_z = z_allperson_aved
+
+screen_x_std =x_std_aved
+screen_y_std =y_std_aved
+screen_z_std =z_std_aved
+
+activities = [1,3]
+
+x_allperson_averaged ={}
+y_allperson_averaged ={}
+z_allperson_averaged ={}
+
+x_allperson_std ={}
+y_allperson_std ={}
+z_allperson_std ={}
+
+x_allperson_averaged[1] = open_close_x
+x_allperson_averaged[3] = screen_int_x
+
+
+y_allperson_averaged[1]= open_close_y
+y_allperson_averaged[3]= screen_int_y
+
+z_allperson_averaged[1]= open_close_z
+z_allperson_averaged[3]= screen_int_z
+
+x_allperson_std[1] = open_close_x_std
+x_allperson_std[3] = screen_x_std
+
+y_allperson_std[1] = open_close_y_std
+y_allperson_std[3] = screen_y_std
+
+z_allperson_std[1] = open_close_z_std
+z_allperson_std[3] = screen_z_std
