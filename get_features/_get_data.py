bcfd3ba5838af3add1cf3d167594d4cc05bd4208
sy2657 <60480017+sy2657@users.noreply.github.com>
2021-11-09 18:56:16 -0500

    Update _get_data.py

--- get_features/_get_data.py
+++ get_features/_get_data.py
@@ -1,3 +1,5 @@
+# get data, get data ang (angle), get data hands
+
 def _get_data(root_dir='data-clean', camera_type='_1.mp4', classes=[]):
 	dataset = []
 	users = []
@@ -52,4 +54,38 @@ def _get_data_ang(root_dir='data-clean', camera_type='_1.mp4', classes=[]):
 			activities.append(classes.index(act))
 			raw_files.append(file)
 		print(act, len(files))
+	return np.array(dataset), np.array(users), np.array(activities), raw_files
+
+def _get_data_hands(root_dir='data-clean', camera_type='_1.mp4', classes=[]):
+	dataset = []
+	users = []
+	activities = []
+	raw_files = []
+	for act in classes:
+		files = glob.glob(f'{root_dir}/refrigerator/' + act + f'/*/*{camera_type}.npy')
+		# files = [f'{root_dir}/data-clean/refrigerator/take_out_item/4/take_out_item_2_1616179391_1.mp4.npy',
+		#          f'{root_dir}/data-clean/refrigerator/take_out_item/4/take_out_item_2_1616179391_1.mp4.npy']
+		for file in files:
+			# print('Processing file', file)
+			user = int(file.split('/')[-2])
+
+			data = []
+			raw_data = np.load(file)
+            # change to specific keypts
+			keypts = [13, 16] # left wrist, right wrist, [5,6,7,8,9,10]
+			#for keypoint_idx in keypts:
+			tmp = get_hand_features(raw_data) #, keypoint=keypoint_idx)
+			data.extend(list(tmp))
+			"""for keypoint_idx, _ in enumerate(keypoints):
+				tmp = get_fft_features(raw_data, keypoint=keypoint_idx)
+				#print("tmp shape is ", np.shape(tmp))
+				data.extend(list(tmp))"""
+			data = np.asarray(data)
+			print("data shape is ", np.shape(data))
+			dataset.append(data)
+			users.append(user)
+			activities.append(classes.index(act))
+			raw_files.append(file)
+		print(act, len(files))
+
 	return np.array(dataset), np.array(users), np.array(activities), raw_files
