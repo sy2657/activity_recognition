6b6ac40fffe398503be9f9be92e4d2cbb24c602f
sy2657 <60480017+sy2657@users.noreply.github.com>
2021-11-09 18:43:56 -0500

    Update _get_data.py

--- get_features/_get_data.py
+++ get_features/_get_data.py
@@ -26,3 +26,30 @@ def _get_data(root_dir='data-clean', camera_type='_1.mp4', classes=[]):
 		print(act, len(files))
 
 	return np.array(dataset), np.array(users), np.array(activities), raw_files
+
+def _get_data_ang(root_dir='data-clean', camera_type='_1.mp4', classes=[]):
+	dataset = []
+	users = []
+	activities = []
+	raw_files = []
+	for act in classes:
+		files = glob.glob(f'{root_dir}/refrigerator/' + act + f'/*/*{camera_type}.npy')
+		for file in files:
+			#print('Processing file', file)
+			user = int(file.split('/')[-2])
+
+			data = []
+			raw_data = np.load(file)
+            # change to specific keypts
+			keypts = [13, 16] # left wrist, right wrist, [5,6,7,8,9,10]
+			#for keypoint_idx in keypts:
+			tmp = get_ang_features(raw_data) #, keypoint=keypoint_idx)
+			data.extend(list(tmp))
+			data = np.asarray(data)
+			print("data shape is ", np.shape(data))
+			dataset.append(data)
+			users.append(user)
+			activities.append(classes.index(act))
+			raw_files.append(file)
+		print(act, len(files))
+	return np.array(dataset), np.array(users), np.array(activities), raw_files
