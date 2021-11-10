6f5f32c634ed14c87cd7e3e5f61ec90665dd08d4
sy2657 <60480017+sy2657@users.noreply.github.com>
2021-11-09 19:02:52 -0500

    Update get_data.py

--- get_features/get_data.py
+++ get_features/get_data.py
@@ -6,3 +6,13 @@ def get_data_orig(root_dir, cameras=[], classes=[]):
 		history[camera] = (dataset, users, activities, raw_files)
 	print("history shape is ", np.shape(history))
 	return history
+
+def get_data_hands(root_dir, cameras=[], classes=[]):
+	history = {}
+	for i, camera in enumerate(cameras):
+		dataset, users, activities, raw_files = _get_data_hands(root_dir, camera_type=camera, classes=classes)
+		#dataset, users, activities, raw_files = get_moment_data(root_dir, camera_type=camera, classes=classes)
+		history[camera] = (dataset, users, activities, raw_files)
+
+	return history
+
