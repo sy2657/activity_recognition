d323a921e162200aea84538942ee2f0c14a5c00d
sy2657 <60480017+sy2657@users.noreply.github.com>
2022-02-11 17:54:57 -0800

    Update extract_frames_100_with_coords.py

--- iot_2022/extract_frames_100_with_coords.py
+++ iot_2022/extract_frames_100_with_coords.py
@@ -1,3 +1,29 @@
+# change previous function to have bins and count the entries in each bin (of width and height of 20 units)
+
+# create bins within area of 0 to 1280 columns and 0 to 720 rows: divide by 20 
+bin_limits = []
+bin_map = {}
+bin_orig_map = {}
+ind=0
+
+for i in range(66): # 1280 total
+  for j in range(38): # 720 total
+    i_lim_low = i*(20)
+    i_lim_high = (i+1)*(20)
+    j_lim_low = j*(20)
+    j_lim_high = (j+1)*(20)
+    ij_low = (i_lim_low, j_lim_low)
+    bin_limits.append(ij_low)
+    bin_map[ij_low] = ind
+    bin_orig_map[(i,j)] = ind
+
+    ind = ind+1
+
+from scipy import stats
+
+binx = 20*np.arange(65)
+biny = 20*np.arange(37)
+
 # combine into this function
 def best_100_frame_seg(grey_frames):
   # step 1 
@@ -76,4 +102,25 @@ def best_100_frame_seg(grey_frames):
 
     if len_coords> 10: # nonzero change ; len_coords is how many 
       print(len_coords)
+      # change the procedure to determine if the frame is right
+      coords_x = frame_coords_x[frame_no]
+      coords_y = frame_coords_y[frame_no]
+      sb = stats.binned_statistic_2d(coords_x, coords_y, None, 'count', bins=[binx, biny], expand_binnumbers=True)
+      bin_arrs = sb[3]
+
+      bin_x = bin_arrs[0]
+      bin_y = bin_arrs[1]
+
+      bin_histogram = {}
+
+      for ind in range(len(bin_x)):
+        pair = (bin_x[ind], bin_y[ind])
+        #print("pair is ", pair)
+        orig_index = bin_orig_map[pair]
+        if bin_histogram.get(orig_index) ==None:
+          bin_histogram[orig_index]=1
+        else:
+          bin_histogram[orig_index]= bin_histogram[orig_index]+1
+      # if the counts within bin_histogram are greater than 100
+      
       return(frame_no, frame_coords, frame_coords_x, frame_coords_y)
